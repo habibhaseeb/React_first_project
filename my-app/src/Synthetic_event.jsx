188fb39add48e036461015cf41d2de49ac3cc882
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2023-09-01 22:01:09 +0500

    Create Synthetic_event.jsx

---
+++ my-app/src/Synthetic_event.jsx
@@ -0,0 +1,12 @@
+# JAVASCRIPT SYNTHETIC_EVENT
+import React from 'react'
+function my_button(){
+    const handleClick = ( event ) =>{ 
+        event.preventDefault();
+        alert("Button is clicked!") 
+  }
+  return (
+    <button onClick={handleClick}>Click me</button>
+   );   
+}
+export default my_button;
